c33aee59ceac314233f03e16e5292913405c593b
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-02-23 12:06:54 +0200

    Update Me contracts

--- src/Service.ActiveOrders.Client/AutofacHelper.cs
+++ src/Service.ActiveOrders.Client/AutofacHelper.cs
@@ -9,11 +9,11 @@ namespace Service.ActiveOrders.Client
 {
     public static class AutofacHelper
     {
-        public static void RegisterActiveOrdersClients(this ContainerBuilder builder, string balancesGrpcServiceUrl, IMyNoSqlSubscriber myNoSqlSubscriber)
+        public static void RegisterActiveOrdersClients(this ContainerBuilder builder, string activeOrdersGrpcServiceUrl, IMyNoSqlSubscriber myNoSqlSubscriber)
         {
             var subs = new MyNoSqlReadRepository<OrderNoSqlEntity>(myNoSqlSubscriber, OrderNoSqlEntity.TableName);
 
-            var factory = new ActiveOrdersClientFactory(balancesGrpcServiceUrl, subs);
+            var factory = new ActiveOrdersClientFactory(activeOrdersGrpcServiceUrl, subs);
 
             builder.RegisterInstance(factory.ActiveOrderService()).As<IActiveOrderService>().SingleInstance();
 
