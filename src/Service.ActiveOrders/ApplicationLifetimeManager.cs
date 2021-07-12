53ac5d1775347c6811a7ee1d8d449205a45ca593
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-07-12 15:31:43 +0300

    fix issue with start api

--- src/Service.ActiveOrders/ApplicationLifetimeManager.cs
+++ src/Service.ActiveOrders/ApplicationLifetimeManager.cs
@@ -8,27 +8,21 @@ namespace Service.ActiveOrders
     public class ApplicationLifetimeManager : ApplicationLifetimeManagerBase
     {
         private readonly ILogger<ApplicationLifetimeManager> _logger;
-        private readonly MyServiceBusTcpClient _client;
 
-        public ApplicationLifetimeManager(IHostApplicationLifetime appLifetime, ILogger<ApplicationLifetimeManager> logger, MyServiceBusTcpClient client)
+        public ApplicationLifetimeManager(IHostApplicationLifetime appLifetime, ILogger<ApplicationLifetimeManager> logger)
             : base(appLifetime)
         {
             _logger = logger;
-            _client = client;
         }
 
         protected override void OnStarted()
         {
             _logger.LogInformation("OnStarted has been called.");
-            _client.Start();
-            _logger.LogInformation("MyServiceBusTcpClient is started.");
         }
 
         protected override void OnStopping()
         {
             _logger.LogInformation("OnStopping has been called.");
-            _client.Stop();
-            _logger.LogInformation("MyServiceBusTcpClient is stopped.");
         }
 
         protected override void OnStopped()
